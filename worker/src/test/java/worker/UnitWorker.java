a64c921b65195bf1f54c51e7247d6db82684a016
Gourav Shah <gs@initcron.org>
2021-03-30 13:39:25 +0530

    initial import

---
+++ worker/src/test/java/worker/UnitWorker.java
@@ -0,0 +1,25 @@
+import org.junit.jupiter.api.Test;
+ 
+class UnitWorker {
+ 
+    @Test
+    void sample1() {
+ 
+    }
+
+
+    @Test
+    void sample2() {
+
+    }
+
+    @Test
+    void sample3() {
+
+    }
+
+    @Test
+    void sample4() {
+
+    }
+}
